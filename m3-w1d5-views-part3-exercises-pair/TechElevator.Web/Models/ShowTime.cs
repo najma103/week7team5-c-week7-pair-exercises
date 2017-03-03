8615a1b88dac8799358246bec2a72ff12a8a5224
John Fulton <john@TechElevator.com>
2017-03-02 19:39:04 -0500

    m3-w1d5-views-part3-exercises-pair

---
+++ m3-w1d5-views-part3-exercises-pair/TechElevator.Web/Models/ShowTime.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TechElevator.Web.Models
+{
+    public class ShowTime
+    {
+        public string Theatre { get; set; }
+        public DateTime StartTime { get; set; }
+    }
+}
